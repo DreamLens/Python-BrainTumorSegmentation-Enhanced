cc905da7662f8b79cacbcd718a7e03ffc059ab9a
DreamLens <DreamLen98@proton.me>
2022-04-18 18:26:52 +0000

    Cleanup of code

--- braintumorseg_script.py
+++ braintumorseg_script.py
@@ -130,3 +130,5 @@ class Ui_MainWindow(object):
                                   "    border: 2px solid #00c6fb;\n"
                                   "background-color:#005bea;\n"
                                   "width:171px;\n"
+                                  "height:61px;")
+        self.addcol.setObjectName("addcol")
