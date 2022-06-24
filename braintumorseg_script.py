2ecf04c41774ad5aa41214659faf04b6943c2a03
DreamLens <DreamLen98@proton.me>
2022-06-24 10:46:41 +0000

    Project created

--- braintumorseg_script.py
+++ braintumorseg_script.py
@@ -139,3 +139,4 @@ class Ui_MainWindow(object):
                                   "   border-radius: 20px;\n"
                                   "    border: 2px solid #00c6fb;\n"
                                   "background-color:#005bea;\n"
+                                  "width:171px;\n"
