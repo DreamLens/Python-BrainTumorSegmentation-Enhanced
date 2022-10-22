b58c05e28850aba4e4db4ef59ee55ed32b70e3ea
DreamLens <DreamLen98@proton.me>
2022-10-22 18:14:26 +0000

    merge in jb-mr2-release history after reset to master

--- braintumorseg_script.py
+++ braintumorseg_script.py
@@ -204,3 +204,7 @@ class Ui_MainWindow(object):
         self.savimg.clicked.connect(self.saving)
 
     def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Tumor Segmentation Panel"))
+        self.slctimg.setText(_translate("MainWindow", "Select Image"))
+        self.rgbtgray.setText(_translate("MainWindow", "Bilateral Filter"))
